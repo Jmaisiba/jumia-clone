c277ccbac8a37625a68a78615c49ba2016d5b9ed
Jmaisiba <jmaisiba@gmail.com>
2025-07-27 21:50:47 +0300

    top hero section partial completion

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+const images = [
+    // "/assets/img/categories-item1.gif",
+    "/assets/img/categories-item2.jpg",
+    "/assets/img/categories-item3.jpg",
+    "/assets/img/categories-item4.jpg",
+    "/assets/img/categories-item5.jpg",
+    // "/assets/img/categories-item6.gif",
+];
+
+let currentIndex = 0;
+const showcase = document.getElementById("imageShowcase");
+
+function changeBackground(){
+    // showcase.style.backgroundImage = `url('${images[currentIndex]}')`;
+    showcase.style.backgroundImage =  `url('${images[currentIndex]}')`;
+    currentIndex = (currentIndex+1) % images.length;
+
+}
+
+changeBackground();
+setInterval(changeBackground, 5000);
+
